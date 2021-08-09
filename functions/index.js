a2fee2ca90d7b017eab35c717a7bd33ed9662e03
chris01b <biddlechris0@gmai.com>
2021-08-09 00:49:05 -0400

    Added functionality for multiple codes at once

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,11 @@ const cors = require('cors')({ origin: true });
 const axios = require('axios');
 const querystring = require('querystring');
 const chance = require('chance').Chance();
+const PastebinAPI = require('pastebin-js');
+const pastebinSecret = functions.config().keys.pastebin;
+const pastebin = new PastebinAPI(pastebinSecret);
 const endpointSecret = functions.config().keys.signing;
+const priceId = functions.config().keys.priceId;
 
 admin.initializeApp();
 
@@ -18,7 +22,12 @@ exports.checkout_sessions = functions.https.onRequest((req, res) => {
           mode: 'payment',
           payment_method_types: ['card'],
           line_items: [{
-            price: 'price_1JM2QLCAIbay177QDnjie75G',
+            price: priceId,
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+              maximum: 99
+            },
             quantity: 1,
           }],
           success_url: `${req.headers.origin}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
@@ -36,51 +45,85 @@ exports.checkout_sessions = functions.https.onRequest((req, res) => {
 });
 
 function generatePhoneNumber() {
-  const unformattedNumber = chance.phone({ formatted: false });
-  const formattedNumber = unformattedNumber.substr(0,3) +
-    '-' + unformattedNumber.substr(3,3) + '-' + unformattedNumber.substr(6);
-  const request = {
-    url: 'https://api.andpizza.com/handlers/phone-offers/gxresolve',
-    method: 'POST',
-    data: 'phone=' + formattedNumber,
-    headers: {
-      "accept": "*/*",
-      "accept-language": "en-US,en;q=0.9",
-      "cache-control": "max-age=0",
-      "content-type": "application/x-www-form-urlencoded",
-      "sec-ch-ua": "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-fetch-dest": "document",
-      "sec-fetch-mode": "navigate",
-      "sec-fetch-site": "same-site",
-      "sec-fetch-user": "?1",
-      "upgrade-insecure-requests": "1"
-    }
-  };
-  return axios(request).catch(err => {
-    let res = err.response;
-    let params = querystring.parse(res.request.path.split("?").pop());
-    if (params.success === 'true') {
-      return(params["offers[0]"]);
-    } else if (params.error === 'Invalid phone number or configuration') {
-      return generatePhoneNumber();
-    } else {
-      return new Promise((resolve, reject) => reject(params));
-    }
-  })
+  return new Promise((resolve, reject) => {
+    const unformattedNumber = chance.phone({ formatted: false });
+    const formattedNumber = unformattedNumber.substr(0,3) +
+      '-' + unformattedNumber.substr(3,3) + '-' + unformattedNumber.substr(6);
+    const request = {
+      url: 'https://api.andpizza.com/handlers/phone-offers/gxresolve',
+      method: 'POST',
+      data: 'phone=' + formattedNumber,
+      headers: {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "content-type": "application/x-www-form-urlencoded",
+        "sec-ch-ua": "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-site",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1"
+      }
+    };
+    axios(request).catch(err => {
+      let res = err.response;
+      let params = querystring.parse(res.request.path.split("?").pop());
+      if (params.success === 'true') {
+        resolve(params["offers[0]"]);
+      } else if (params.error === 'Invalid phone number or configuration') {
+        generatePhoneNumber().then(code => resolve(code));
+      } else {
+        reject(params);
+      }
+    });
+  });
 }
 
-function fulfillOrder(event) {
-  generatePhoneNumber().then(async code => {
-    console.log("Fulfilling order:", event.data.object.id, 'with code:', code);
-    await admin.database().ref('/orders').set({
-      [event.data.object.id]: {
-        code: code,
+function* generatePhoneNumberIterable(quantity) {
+  let iterationCount = 0;
+  for (let i = 0; i < quantity; i += 1) {
+    iterationCount++;
+    yield generatePhoneNumber();
+  }
+  return iterationCount;
+}
+
+function getOrderQuantity(event) {
+  return new Promise((resolve, reject) => {
+    stripe.checkout.sessions.listLineItems(event.data.object.id, { limit: 100 }, (err, lineItems) => {
+      if (err) { reject(err) }
+      resolve(lineItems.data[0].quantity);
+    });
+  });
+}
+
+function fulfillOrder(event, quantity) {
+  // Run this once every generatePhoneNumber promise resolves
+  Promise.all(generatePhoneNumberIterable(quantity)).then(codes => {
+    // Generate a pastebin link to host the codes
+    if (quantity !== 1) {
+      pastebin.createPaste(codes.join('\n'), event.data.object.id, 'text', 1, 'N')
+      .then(data => {
+        console.log(data);
+        // Post the codes and its pastebin url
+        admin.database().ref('/orders').child(event.data.object.id).set({
+            codes: codes,
+            pasteUrl: data,
+            created: event.created,
+            customerId: event.data.object.customer,
+            ...event.data.object.customer_details
+          }).catch(e => console.error(e));
+      }).fail(e => console.log(e));
+    } else { // Just post the one code
+      admin.database().ref('/orders').child(event.data.object.id).set({
+        code: codes[0],
         created: event.created,
         customerId: event.data.object.customer,
         ...event.data.object.customer_details
-      }
-    });
+      }).catch(e => console.error(e));
+    }
   }).catch(e => console.error(e));
 }
 
@@ -101,17 +144,18 @@ exports.generateCode = functions.https.onRequest((req, res) => {
 
     switch (event.type) {
       case 'checkout.session.completed': {
-        if (event.data.object.payment_status === 'paid') {
-          fulfillOrder(event);
-        }
+        getOrderQuantity(event)
+        .then(quantity => fulfillOrder(event, quantity))
+        .catch(e => console.error(e));
         break;
       }
       case 'checkout.session.async_payment_succeeded': {
-        fulfillOrder(event);
-        break;
-      }
-      default: {
-        break;
+        if (event.data.object.payment_status === 'paid') {
+          getOrderQuantity(event)
+          .then(quantity => fulfillOrder(event, quantity))
+          .catch(e => console.error(e));
+          break;
+        }
       }
     }
   });
@@ -126,9 +170,12 @@ exports.getCode = functions.https.onRequest((req, res) => {
     .once('value', snapshot => {
       try {
         const data = snapshot.val();
-        console.log(data);
-        return res.status(200).send({ code: data.code });
-      } catch (e) { console.error('Malformed Database') }
-    });
+        if (data.hasOwnProperty('code')) {
+          return res.status(200).send({ code: data.code });
+        } else if (data.hasOwnProperty('pasteUrl')) {
+          return res.status(200).send({ pasteUrl: data.pasteUrl });
+        }
+      } catch (e) { console.error(e) }
+    }).catch(e => console.error(e));
   });
 });
